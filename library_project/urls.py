e8be5bc98cd849874d205a4e69a2cf23df3065bd
dmswl0311 <dmswl_0311@naver.com>
2021-01-05 02:05:23 +0900

    index 페이지 추가

--- library_project/urls.py
+++ library_project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import mainapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', mainapp.views.index,name='index'),
 ]
