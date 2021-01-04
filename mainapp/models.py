dee1c43d3e87a10a3b66983b18efa468a05062b1
dmswl0311 <dmswl_0311@naver.com>
2021-01-05 02:07:12 +0900

    LibraryList 모델 추가

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class LibraryList(models.Model):
+    ISBN = models.IntegerField(default=0)
+    year = models.IntegerField(default=0)
+    book_name = models.TextField(blank=True)
+    author = models.TextField(blank=True)
+    explain = models.TextField(blank=True)
+    url = models.TextField(blank=True)
+    gender = models.CharField(max_length=10)
+    age = models.IntegerField(default=0)
+    category_rank = models.IntegerField(default=0)
+    rank = models.IntegerField(default=0)
+    category_name = models.TextField(blank=True)
