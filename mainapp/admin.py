219e58dfe1d0a6b244f54eb164e4fbd4953f968d
dmswl0311 <dmswl_0311@naver.com>
2021-01-05 02:10:36 +0900

    admin에 import,export 기능 추가

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,4 +1,7 @@
+from import_export.admin import ImportExportModelAdmin
 from django.contrib import admin
 from .models import LibraryList
 
-admin.site.register(LibraryList)
+@admin.register(LibraryList)
+class LibraryListAdmin(ImportExportModelAdmin):
+    pass
