8b7bf6b9e14a3b0d223321dbac9dfbdb872f5566
dmswl0311 <dmswl_0311@naver.com>
2021-01-07 21:42:16 +0900

    allowed_hosts 수정

--- library_project/settings.py
+++ library_project/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY ='%d9721na+4ysg3=v484xfjd=y_i$o#)$-_&9smlo&&eqqif83-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = []
 
 
 # Application definition
