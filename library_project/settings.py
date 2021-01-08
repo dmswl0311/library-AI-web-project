4d66634c6a4fc4b64266babbb92f37380139f6cd
dmswl0311 <dmswl_0311@naver.com>
2021-01-08 19:28:20 +0900

    alert 추가, debug false로 수정

--- library_project/settings.py
+++ library_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY ='%d9721na+4ysg3=v484xfjd=y_i$o#)$-_&9smlo&&eqqif83-'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
